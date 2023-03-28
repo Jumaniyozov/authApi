319466734b0aae7b0ed21477e60878645edf4e3e
jumaniyozov <jumaniyozovse@gmail.com>
2023-03-28 18:03:16 +0500

    middlewares

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
 const path = require("path");
 const { logger } = require("./middleware/logEvents");
+const errorHandler = require("./middleware/errorHandler");
 const PORT = process.env.PORT || 3500;
 
 app.use(express.urlencoded({ extended: false }));
@@ -10,6 +12,25 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, "/public")));
 
 app.use(logger);
+
+const whitelist = [
+  "https://www.youtube.com",
+  "http://127.0.0.1:5500",
+  "http://localhost:3500",
+];
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 
 app.get("^/$|index(.html)?", (req, res) => {
   res.sendFile(path.join(__dirname, "views", "index.html"));
@@ -26,6 +47,8 @@ app.get("^/old-page(.html)?", (req, res) => {
 app.get("/*", (req, res) => {
   res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`);
